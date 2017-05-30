25685981e8b75bd903cbe92887cae96c792aed8b
Aman Agarwal <aman15012@iiitd.ac.in>
2017-05-30 23:46:16 +0530

    try commit

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 //This is the main file that includes all the required files.
 
-//Including the toggle switch html:
+//try
 var header_elements = document.getElementsByClassName("header_contents");
-header_elements[0].innerHTML += "<div w3-include-html=\"https://github.com/aman15012/seeQuora/blob/master/switch.html\"></div><script> w3.includeHTML(); </script>";
+header_elements[0].innerHTML += "<div w3-include-html=\"https://github.com/aman15012/seeQuora/blob/master/try.html\"></div><script> w3.includeHTML(); </script>";
 
+
+//Including the toggle switch html:
+/*var header_elements = document.getElementsByClassName("header_contents");
+header_elements[0].innerHTML += "<div w3-include-html=\"https://github.com/aman15012/seeQuora/blob/master/switch.html\"></div><script> w3.includeHTML(); </script>";
+*/
 //Including see-quora.js 
 var head_in_html = document.getElementsByTagName("head");
 head_in_html[0].innerHTML += "<script src=\"https://github.com/aman15012/seeQuora/blob/master/see-quora.js\">";
