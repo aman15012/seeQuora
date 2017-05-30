8e06d223b368f4101a4175fba68068d0481d64e1
Aman Agarwal <aman15012@iiitd.ac.in>
2017-05-30 23:41:14 +0530

    third commit

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
+//This is the main file that includes all the required files.
+
 //Including the toggle switch html:
-
 var header_elements = document.getElementsByClassName("header_contents");
-header_elements[0].innerHTML += "<div w3-include-html=\"switch.html\"></div><script> w3.includeHTML(); </script>";
+header_elements[0].innerHTML += "<div w3-include-html=\"https://github.com/aman15012/seeQuora/blob/master/switch.html\"></div><script> w3.includeHTML(); </script>";
 
-//Including other javascript elments - 1.see-quora.js 2.an event listner on scrolling
+//Including see-quora.js 
+var head_in_html = document.getElementsByTagName("head");
+head_in_html[0].innerHTML += "<script src=\"https://github.com/aman15012/seeQuora/blob/master/see-quora.js\">";
 
-
-
-
-/*1. add toggle switch
-2. add scroll event listner
-3. javascript file*/
+//Including an event listner for scrolling
+var body_in_html = document.getElementsByTagName("body");
+body_in_html[0].setAttribute("onscroll","scroll_response()");
--- see-quora.js
+++ see-quora.js
@@ -1,3 +1,16 @@
+var state = 0;
+function handleClick(cb) {
+  if(cb.checked)
+  {
+  	state = 1;
+  	see();
+  }
+  else
+  {
+  	state = 0;
+  	no_see();
+  }
+}
 var see = function()
 {
 	var x = document.getElementsByClassName("count hide_in_feed");
@@ -14,8 +27,15 @@ var no_see = function()
 		x[i].style.display = "none";
 	}
 }
-var pos = document.body.scrollTop / (document.body.scrollHeight - document.body.clientHeight);
-if(pos>0.8 || document.body.scrollTop == 0)
+function scroll_response()
 {
-	see();
+	var pos = document.body.scrollTop / (document.body.scrollHeight - document.body.clientHeight);
+	if(state)
+	{
+		if(pos>0.8 || document.body.scrollTop == 0)
+		{
+			see();
+		}
+	}
+	
 }
