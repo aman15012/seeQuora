0ba61aea82ce1cf99064b47a0e75344d1eae8922
Aman Agarwal <aman15012@iiitd.ac.in>
2017-05-30 23:29:43 +0530

    second commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//Including the toggle switch html:
+
+var header_elements = document.getElementsByClassName("header_contents");
+header_elements[0].innerHTML += "<div w3-include-html=\"switch.html\"></div><script> w3.includeHTML(); </script>";
+
+//Including other javascript elments - 1.see-quora.js 2.an event listner on scrolling
+
+
+
+
+/*1. add toggle switch
+2. add scroll event listner
+3. javascript file*/
